715da32080c3a26bba37900cd213cf9786068ad0
ilyass <eude1986@gmail.com>
2021-03-04 21:35:20 +0100

    first commit

---
+++ com.ily.gitTraining/src/main/java/gitTraining/com/ily/gitTraining/App.java
@@ -0,0 +1,13 @@
+package gitTraining.com.ily.gitTraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
